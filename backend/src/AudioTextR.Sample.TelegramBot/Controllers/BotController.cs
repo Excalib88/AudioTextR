7a4913a6ff21e7f92b41c4dd2e8394d10d222984
Damir Mansurov <rave9373@gmail.com>
2019-11-30 21:45:16 +0300

    added telegrambot sample

---
+++ backend/src/AudioTextR.Sample.TelegramBot/Controllers/BotController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using AudioTextR.Core.Abstractions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+namespace AudioTextR.Sample.TelegramBot.Controllers
+{
+    [Route("bot")]
+    [ApiController]
+    public class BotController : Controller
+    {
+        private readonly ITelegramBotClient _client;
+        private readonly ISpeechService _speechService;
+
+        public BotController(ITelegramBotClient client, ISpeechService speechService)
+        {
+            _client = client;
+            _speechService = speechService;
+        }
+
+        [HttpPost("update")]
+        public async Task Post([FromBody]Update update)
+        {
+            if (update == null) return;
+
+            var message = update.Message;
+
+            if (message?.Type == MessageType.Audio)
+            {
+                var text = "";
+                if(message.Audio.Duration < 20)
+                {
+                    var audioMessageId = message.Audio.FileId;
+
+                    using (var audioStream = new MemoryStream())
+                    {
+                        await _client.GetInfoAndDownloadFileAsync(audioMessageId, new MemoryStream());
+
+                        var result = await _speechService.Recognize(audioStream);
+                        text = result.Text;
+                    }                        
+                }
+
+                await _client.SendTextMessageAsync(message.Chat.Id, text);
+            }
+
+        }
+    }
+}
---
+++ backend/src/AudioTextR.Sample.TelegramBot/Startup.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using AudioTextR.Core.Extensions;
+using AudioTextR.Core.Abstractions.Models;
+using AudioTextR.Core.Models;
+using Telegram.Bot;
+
+namespace AudioTextR.Sample.TelegramBot
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddSingleton<ITelegramBotClient>(new TelegramBotClient(Configuration["TelegramApi:Token"]));
+
+            var recognizeModel = new WitAiModel
+            {
+                BaseAddress = new Uri(Configuration["WitAi:BaseAddress"]),
+                ServerToken = Configuration["WitAi:Token"]
+            };
+
+            services.AddAudioTextR(recognizeModel);
+        }
+
+        public async void Configure(IApplicationBuilder app, IHostingEnvironment env, IServiceProvider serviceProvider)
+        {
+            await serviceProvider.GetService<ITelegramBotClient>().SetWebhookAsync("https адрес от Ngrok сервиса");
+            app.UseMvc();
+        }
+    }
+}
